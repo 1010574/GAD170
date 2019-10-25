f3a4f02b2ac967119daf8129bc3891acfaa1c855
kylelyon <1010574@student.sae.edu.au>
2019-10-25 17:34:09 +1000

    Completed Commenting Tutorial!

---
+++ GAD170_EssentialsTutorial/Assets/Scenes/Comment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comment : MonoBehaviour
+{
+
+    //This is a single line comment
+
+    /// <summary>
+    /// myFirstBool is a bool that is my first.
+    /// </summary>
+    [Tooltip("A bool used for my first")]
+    public bool myFirstBool = true;
+
+    [Header("Player Name")]
+    public string myFirstString = "SomeString";
+
+    [Space(30)]
+    public int myFirstInt = 1;
+
+    /* Multiline Comment
+     * this is a multiline comment!
+     * you will notice when I press enter I get a new line!
+     */
+
+    #region Functions - Holds all the functions in my game
+
+    /// <summary>
+    /// this is a start function. It is called on the first frame of the game
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myFirstBool = false;
+    }
+
+    #endregion
+}
