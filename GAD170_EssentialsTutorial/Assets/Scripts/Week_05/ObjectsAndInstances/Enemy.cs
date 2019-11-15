140893abe2f3a3185597e143e7030f283404997e
kylelyon <1010574@student.sae.edu.au>
2019-11-15 10:26:08 +1000

    Completed Objects and Instances tutorial.

--- GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectsAndInstances/Enemy.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectsAndInstances/Enemy.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string myFirstName; // enemies first name
+    public string myLastName; // enemies last name
+    public int health; // enemies health    
+    public int mana; // enemies mana
 
-    // Update is called once per frame
-    void Update()
+    private string m_fullName; // enemies full name, private variable. Cant be seen in EnemyManager
+
+    /// <summary>
+    /// Sets the instance enemy class with some default values.
+    /// </summary>
+    public void SetUpEnemy()
     {
-        
+        myFirstName = "Kyle";
+        myLastName = "Lyon";
+        health = 100;
+        mana = 100;
+        m_fullName = myLastName + " " + myLastName;
     }
 }
--- GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectsAndInstances/EnemyManager.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectsAndInstances/EnemyManager.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Examples of USING namespaces
 
-public class EnemyManager : MonoBehaviour
+// This is a namespace, we can encapsulate our code around it to 
+// tell people that this class is contained within a certain namespace grouping our classes together.
+namespace Level01
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyManager : MonoBehaviour
     {
-        
+        public Enemy enemy01; // An Instance of Enemy Script
+        public Enemy enemy02; // Another Instance of Enemy script
+
+        public EnemyContactDetails enemyDetails; // A data storage class, to contain info about enemy.
+
+        public MainMenu mainMenu; // Another data storage class to contain info about main menu
+        public SettingsMenu settingsMenu; // Another data storage class to contain info about settings
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            // this is how I reference my instance of the enemy class and access variables and functions of it. 
+            enemy01.SetUpEnemy();
+            enemy01.myFirstName = "Poopybutthole";
+
+            // another example of accessing another instance.
+            enemy02.SetUpEnemy();
+            enemy02.health = 50;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // System.Serializable lets unity know that I want this visible in the inspector. 
+    // this is also an example of a data storage class, where no runtime instance is required to make it work.
+    [System.Serializable]
+    public class EnemyContactDetails
     {
-        
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+
+    [System.Serializable]
+    public class MainMenu
+    {
+        public Button startButton;
+
+        public void PlayButton()
+        {
+            Debug.Log("Play The Game");
+        }
+    }
+
+    [System.Serializable]
+    public class SettingsMenu
+    {
+        public Text settings;
     }
 }
