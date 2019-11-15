b0a88c7eb2149714828088edc0f6797476030126
kylelyon <1010574@student.sae.edu.au>
2019-11-15 10:53:48 +1000

    completed ScriptToFind tutorial.

--- GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectReferencing/ScriptFinder.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectReferencing/ScriptFinder.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ScriptFinder : MonoBehaviour
 {
+    public static ScriptFinder ScriptFinderInstance; // an example of a static instance reference.
+
+    private ScriptToFind scriptToFind; // a reference to the object transfor that has our script to find
+    public Transform objectTransform; // a reference to the script to find.
+
+    private void Awake()
+    {
+        ScriptFinderInstance = this; // "This" keyword refers to THIS instance of the script. 
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scriptToFind = GetComponent<ScriptToFind>(); // this one will search the object that THIS script is attached to.
+        scriptToFind = objectTransform.GetComponent<ScriptToFind>(); // this one will search objectTransform reference for the script that it is attached to
+        scriptToFind = FindObjectOfType<ScriptToFind>(); // This one will find the first instance of the "ScriptToFind" script.
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectReferencing/ScriptToFind.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_05/ObjectReferencing/ScriptToFind.cs
@@ -7,7 +7,7 @@ public class ScriptToFind : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
