e7e1f9d4a1f1fff8689ba461a496e6ec9cc17cb9
kylelyon <1010574@student.sae.edu.au>
2019-11-25 16:55:01 +1000

    Completed FruitSaladV2 tutorial.

--- GAD170_EssentialsTutorial/Assets/Scripts/Week_06/FruitSaladV2/Fruit.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_06/FruitSaladV2/Fruit.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string fruitName; // the name of the fruit.
+    public int numberOfSlices; // Number of slices to cut fruit into.
+    public Color fruitColor; // Color of fruit
+    public bool hasSeeds = false; // Does the fruit have seeds?
+
+    /// <summary>
+    /// Function to set up fruit with some default values.
+    /// </summary>
+    public void SetupFruit()
     {
-        
+        if (GetComponent<MeshRenderer>())
+        {
+            GetComponent<MeshRenderer>().material.color = fruitColor;
+        }
+        else
+        {
+            Debug.Log("No Mesh renderer component");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Cuts the fruit into number of slices dictated by the instance.
+    /// </summary>
+    public void ChopFruit()
     {
-        
+        string finalMessage = hasSeeds == true ? "But I had to be careful as it had seeds" : "And it was delicious because it had no seeds";
+
+        Debug.Log(string.Format("Cut the {0} into {1} slices, {2}", fruitName, numberOfSlices, finalMessage));
     }
+
+
+
+  
 }
--- GAD170_EssentialsTutorial/Assets/Scripts/Week_06/FruitSaladV2/FruitBowlV2.cs
+++ GAD170_EssentialsTutorial/Assets/Scripts/Week_06/FruitSaladV2/FruitBowlV2.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class FruitBowlV2 : MonoBehaviour
 {
+    public List<Fruit> allFruit = new List<Fruit>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // an example of converting an array into a list using system.linq
+        allFruit = FindObjectsOfType<Fruit>().ToList();
+
+
+        // An example of converting an array into a list.
+        Fruit[] tempArray = FindObjectsOfType<Fruit>();
+        List<Fruit> templist = new List<Fruit>();
+        for(int i =0; i< tempArray.Length; i++)
+        {
+            templist.Add(tempArray[i]);
+        }
+
+        // This iterates through my list of fruit
+        for(int i=0; i <allFruit.Count; i++)
+        {
+            // for each element of fruit, set it up.
+            allFruit[i].SetupFruit();
+            // for each element of fruit, chop it.
+            // An example of limiting what gets chopped..
+            if(allFruit[i].fruitName == "Apple")
+            {
+                allFruit[i].ChopFruit();
+
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
