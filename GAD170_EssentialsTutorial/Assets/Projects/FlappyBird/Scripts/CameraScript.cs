79d912a2eb09a098545ab3b771c02457147cb886
kylelyon <1010574@student.sae.edu.au>
2019-12-03 23:24:36 +1000

    Finished first video tutorial of flappybird.

---
+++ GAD170_EssentialsTutorial/Assets/Projects/FlappyBird/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to handle the camera movement of our camera
+/// </summary>
+public class CameraScript : MonoBehaviour
+{
+    public Vector3 offset; // offsets the transform, when moving by x,y,z
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moves the camera along with the player, only taking into account the players X position. We also add an offset.
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z) + offset;
+    }
+}
--- GAD170_EssentialsTutorial/Assets/Projects/FlappyBird/Scripts/PlayerInput.cs
+++ GAD170_EssentialsTutorial/Assets/Projects/FlappyBird/Scripts/PlayerInput.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+
+    public KeyCode jumpKey = KeyCode.Space; // Keycode that makes the bird flap
+    private Rigidbody myRigidBody; // reference to rigidbody component.
+    public float jumpForce = 375; // force applied when space bar is pressed.
+    public float fallingMass = 100; // The mass applied when falling
+    public float jumpingMass = 1; // the mass applied when jumping
+    public float moveSpeed = 5; // The speed we move across the screen.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // checks to see if object has a rigidbody attached..
+        if (GetComponent<Rigidbody>())
+        {
+            // if so, sets myRigidBody to the objects rigidbody.
+            myRigidBody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.Log("No rigidbody on object");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Jump();
+        Move();
         
+    }
+
+    /// <summary>
+    /// Handles jumping mechanic of our flappybird.
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            myRigidBody.mass = jumpingMass;
+            // adds a force to our rigid body on the Y axis, multiplied by jump force.
+            myRigidBody.AddForce(Vector3.up * jumpForce * jumpingMass);
+        }
+        // this sets the rigid body to a higher mass, so we fall faster after jumping.
+        if(myRigidBody.mass != fallingMass)
+        {
+            myRigidBody.mass = fallingMass;
+        }
+    }
+
+    /// <summary>
+    /// Handles the horizontal movement of our flappybird. 
+    /// </summary>
+    private void Move()
+    {
+        // differnt ways to do movement.
+        transform.position += new Vector3(1, 0, 0) * Time.deltaTime * moveSpeed;
+        // Lerp moves fast towards goal position, and then slows down as it gets closer.
+       // transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right, Time.deltaTime);
+        // move towards moves at a constant rate towards a position.
+       // transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.right, Time.deltaTime);
     }
 }
